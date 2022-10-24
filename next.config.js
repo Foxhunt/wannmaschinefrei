89cfda4dff8ea010e92b3744e0d2f3bf8ae7370d
Marco Mojica <marco@mojica.de>
2022-10-25 00:31:22 +0200

    next-pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,32 @@
-/** @type {import('next').NextConfig} */
+/** 
+ * @type {import('next').NextConfig}
+ */
+
+const runtimeCaching = require('next-pwa/cache')
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  runtimeCaching: [
+    ...runtimeCaching,
+    {
+      urlPattern: /\/api\/start/i,
+      handler: 'NetworkFirst',
+      method: 'POST',
+      options: {
+        backgroundSync: {
+          name: 'offlineQueue',
+          options: {
+            maxRetentionTime: 30,
+          }
+        }
+      }
+    }
+  ]
+})
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,40 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <meta name="application-name" content="Wannmaschinefrei" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="apple-mobile-web-app-title" content="Wannmaschinefrei" />
+      <meta name="description" content="Schau ob eine Maschine im Keller frei ist!" />
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="msapplication-TileColor" content="#2B5797" />
+      <meta name="msapplication-tap-highlight" content="no" />
+      <meta name="theme-color" content="#000000" />
+
+      <link rel="icon" type="image/png" sizes="144x144" href="icon-144.png" />
+      <link rel="icon" type="image/png" sizes="512x512" href="icon-512.png" />
+      <link rel="apple-touch-icon" href="/icon-512.png"></link>
+      <link rel="manifest" href="/manifest.json" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Wannmaschinefrei" />
+      <meta property="og:description" content="Schau ob eine Maschine im Keller frei ist!" />
+      <meta property="og:site_name" content="Wannmaschinefrei" />
+      <meta property="og:url" content="https://www.wannmaschinefrei.de/" />
+
+      <meta
+        name='viewport'
+        content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
+      />
+
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
