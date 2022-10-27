d03a0f2a2491c75532badaaf62c75a627bfae6a0
Marco Mojica <marco@mojica.de>
2022-10-27 22:37:43 +0200

    Add NetworkFirst chaching for home

--- next.config.js
+++ next.config.js
@@ -1,16 +1,31 @@
 /** 
  * @type {import('next').NextConfig}
+ * @type {import('next-pwa')}
  */
 
 const runtimeCaching = require('next-pwa/cache')
 
 const withPWA = require("next-pwa")({
   dest: "public",
+  dynamicStartUrl: false,
+  cacheStartUrl: false,
   runtimeCaching: [
+    {
+      urlPattern: /\/$/i,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'home',
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60 // 24 hours
+        },
+        networkTimeoutSeconds: 1/2
+      }
+    },
     ...runtimeCaching,
     {
-      urlPattern: /\/api\/start/i,
-      handler: 'NetworkFirst',
+      urlPattern: /\/api\/start/,
+      handler: 'NetworkOnly',
       method: 'POST',
       options: {
         backgroundSync: {
